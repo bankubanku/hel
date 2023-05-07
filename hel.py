7e78e54f19a1ffee298709b282008406867f99fa
b4nq <wozniczka.jakub@proton.me>
2023-05-07 17:01:54 +0200

    added comments

--- hel.py
+++ hel.py
@@ -14,33 +14,34 @@ DATE_FORMAT_ALT = '%a, %d %b %Y %H:%M:%S %z'
 def get_urls():
     '''Read urls from a file ~/.hel/urls, format it and return 
 
-    Returns:
+    returns:
         urls [list]: list of read urls 
     '''
 
     url_file = os.path.expanduser('~/.hel/urls')
+    urls = []
 
-    f = open(url_file, "r")
-    urls = f.readlines()
-
-    for x in range(len(urls)-1):
-        urls[x] = urls[x].replace('\n', '')
-
+    with open(url_file, "r") as f:
+        for x in f:
+            '''check if given line starts with http to avoid errors when requesting and give an ability to comment'''
+            if not x.startswith('http'):
+                continue
+            '''remove new line escape sign to avoid errors'''
+            x = x.replace('\n', '')
+            urls.append(x)
+    
     return urls
 
 
 def get_source(url):
-    """Return the source code for the provided URL. 
+    """return the source code for the provided URL. 
 
-    Args: 
-        url (string): URL of the page to scrape.
+    args: 
+        url (string): url of the page to scrape.
 
-    Returns:
-        response (object): HTTP response object from requests_html. 
+    returns:
+        soup {bs4.object}: soup of scraped data or 0 if an error occured
     """
-
-    if not url.startswith('http'):
-        return 0
 
     try:
         session = HTMLSession()
@@ -54,28 +55,39 @@ def get_source(url):
         return 0
 
 
-def get_feed(posts):
-    """Read and show feed
-    """
+def get_feed(posts_list):
+    '''get feed from the Internet based on given urls and update the list of posts
+
+    args: 
+        posts_list (list): already saved posts
+
+    returns: 
+        posts_list (list): unsorted list of dictinaries updated by newer feed from the internet
+    '''
 
     urls = get_urls()
 
-    if posts == []:
+    '''check what was publication date of the last saved post and set 1 if there was no last saved post'''
+    if posts_list == []:
         last_pubDate = 1
     else:
-        print(posts[0])
-        last_pubDate = posts[0]['pubDate']
+        print(posts_list[0])
+        last_pubDate = posts_list[0]['pubDate']
 
+    '''go through each source and get needed data'''
     for url in urls:
 
+        '''get soup (bs4) for given source'''
         soup = get_source(url)
 
+        '''skip parsing data if there is no soup to parse'''
         if soup == 0:
             continue
 
         account = soup.find('channel').find('title').text
         items = soup.findAll('item')
 
+        '''write proper data to its place for each item'''
         for item in items:
 
             post = {
@@ -92,11 +104,13 @@ def get_feed(posts):
             post['description'] = item.find('description')
 
             for x in post:
+                '''check if x isn't None and if it isn't make a string out of it'''
                 if x == 'account':
                     continue
                 if post[x] is not None:
                     post[x] = post[x].text
 
+                '''check if pubDate is in the right format and if it isn't change it to this format'''
                 if x == 'pubDate':
                     try:
                         datetime.strptime(post[x], DATE_FORMAT)
@@ -104,24 +118,31 @@ def get_feed(posts):
                         post[x] = datetime.strptime(post[x], DATE_FORMAT_ALT)
                         post[x] = post[x].strftime(DATE_FORMAT)
 
+            '''if pubDate of this post is older or the same as lase saved post then don't add it again'''
             if last_pubDate != 1:
                 if datetime.strptime(last_pubDate, DATE_FORMAT) >= datetime.strptime(post['pubDate'], DATE_FORMAT):
                     break
+                    
+            posts_list.append(post)
 
-            posts.append(post)
-
-    return posts
+    return posts_list
 
 
 def main():
+    '''Get already saved posts'''
     posts_list = posts.get_list()
-    print(posts_list)
+
+    '''Update list of posts by the feed from the Internet'''
     posts_list = get_feed(posts_list)
+
+    '''Sort these posts based on publication date from the newest at the top'''
     posts_list = sorted(posts_list, key=lambda x: datetime.strptime(
         x['pubDate'], DATE_FORMAT), reverse=True)
 
+    '''Save updated list'''
     posts.save_list(posts_list)
 
+    '''Show errors which possibly occured in the meantime'''
     for e in get_source_errs:
         print(e)
 
--- posts.py
+++ posts.py
@@ -8,7 +8,8 @@ JSON_POSTS = expanduser('~/.hel/posts.json')
 def get_list():
     '''get list of posts from json file posts.json
     
-    returns: list of all posts saved in posts.json 
+    returns: 
+        posts (list): all posts saved in posts.json 
     '''
     if exists(JSON_POSTS):
         with open(JSON_POSTS, 'rb') as f:
@@ -21,7 +22,8 @@ def get_list():
 def save_list(posts):
     '''save list of posts as a json file in posts.json
     
-    args: list of dictionaries 
+    args: 
+        posts (list): updated and sorted list of posts data  
     '''
     with open(JSON_POSTS, 'w') as f:
         json.dump(posts, f, indent=4)
