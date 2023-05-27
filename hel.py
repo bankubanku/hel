a65e9711106f7fdaa194e7b16d2d618215a729ae
b4nq <wozniczka.jakub@proton.me>
2023-05-27 21:11:34 +0200

    removed comments and unnecessary code

--- hel.py
+++ hel.py
@@ -14,17 +14,9 @@ def main():
 
     '''Update list of posts by the feed from the Internet'''
     posts_list, get_source_errs = feed.update(posts_list)
-
-    # print(len(posts_list))
-
-    # '''Sort these posts based on publication date from the newest at the top'''
-    # posts_list = sorted(posts_list, key=lambda x: datetime.strptime(
-    #     x['pubDate'], RSS_DATE_FORMAT), reverse=True)
     
     '''Save updated list'''
     json_handling.save_list(posts_list)
-    # with open(JSON_POSTS, 'w') as f:
-        # json.dump(posts_list, f, indent=4)
 
     '''Show errors which possibly occured in the meantime'''
     for e in get_source_errs:
