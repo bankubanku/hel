65a045d851ccfa479d3ccad9bcca6344b80948b2
b4nq <wozniczka.jakub@proton.me>
2023-05-28 13:09:12 +0200

    removed appending old post flaw

--- data_handling.py
+++ data_handling.py
@@ -11,30 +11,28 @@ def is_old(new_time, last_time):
     else:
         return False
 
+
 def get_data(parsed_entries, last_pubDate):
 
     new_posts = []
 
-    for x in range(len(parsed_entries)):
+    for entry in parsed_entries.entries:
         post_data = {
-            'author': None,
-            'title': None,
-            'link': None,
-            'published': None,
-            'description': None
+            'author': parsed_entries.feed.get('title', 'no data'),
+            'title': entry.get('title', 'no data'),
+            'link': entry.get('link', 'no data'),
+            'published': entry.get('published_parsed', gmtime()),
+            'description': entry.get('description',  'no data')
         }
-        post_data['author'] = parsed_entries.feed.get('title', 'no data')
-        post_data['title'] = parsed_entries.entries[x].get('title', 'no data')
-        post_data['link'] = parsed_entries.entries[x].get('link', 'no data')
-        post_data['published'] = parsed_entries.entries[x].get('published_parsed', gmtime())
-        post_data['description'] = parsed_entries.entries[x].get('description',  'no data')
+
+        post_data['published'] = mktime(post_data['published'])
+
         if post_data['description'] == 'no data': 
             post_data['description'] = parsed_entries.entries[x].get('summary',  'no data')
-
+        
         if is_old(post_data['published'], last_pubDate):
             continue
-        else:
-            post_data['published'] = mktime(post_data['published']) #time.mktime(post_data['published'].timetuple())
+
         new_posts.append(post_data)
 
     return new_posts
